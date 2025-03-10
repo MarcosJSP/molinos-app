2876268702b5c1155753f32c0155e806d7ea95be
Marcos Jesús Santana Pérez <32165693+MarcosJSP@users.noreply.github.com>
2025-03-10 22:20:09 +0000

    feat: added icons as react components

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,11 @@
 import { JSX } from 'react'
+import Download from './components/Icons/Download'
 
 function App(): JSX.Element {
   return (
     <>
       <h1 className="text-4xl text-blue-500">Molinos App</h1>
+      <Download className="text-4xl" />
     </>
   )
 }
